7f5300b8ab9d19e35deefef05b59aac7d8f5f116
Ari Juljulian <arijul2@me.com>
2024-03-15 14:24:47 -0700

    back to normal

--- script.js
+++ script.js
@@ -1,163 +1,79 @@
 // Set the dimensions and margins of the graph
-const margin = { top: 10, right: 30, bottom: 50, left: 50 },
-      width = 900 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+const margin = { top: 20, right: 20, bottom: 70, left: 100 },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-// Append the svg object to the body of the page
-const svg = d3.select("#content")
+// Append the svg object to the div called 'bar-chart'
+const svg = d3.select("#bar-chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Read the data from a CSV file
-d3.csv("Argentina Shot Data - Saudi Arabia.csv").then(function(data) {
+// Create a tooltip
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+    
 
-  // Filter data to separate Argentina and Saudi Arabia shots
-  const argentinaData = data.filter(d => d.Squad === "Argentina").map(d => ({ ...d, xG: +d.xG, Minute: +d.Minute }));
-  const saudiData = data.filter(d => d.Squad === "Saudi Arabia").map(d => ({ ...d, xG: +d.xG, Minute: +d.Minute }));
+// Parse the Data
+d3.json('total_world_cup_goals_by_country.json').then(function(data) {
 
-  // Sort data by minute
-  argentinaData.sort((a, b) => a.Minute - b.Minute);
-  saudiData.sort((a, b) => a.Minute - b.Minute);
+  // Sort the data by total goals and take the top 10
+  const top10Data = data.sort((a, b) => d3.descending(a.total_goals, b.total_goals)).slice(0, 10);
 
-  // Calculate cumulative xG
-  let argentinaCumulativeXG = 0, saudiCumulativeXG = 0;
-  argentinaData.forEach(d => { argentinaCumulativeXG += d.xG; d.cumulativeXG = argentinaCumulativeXG; });
-  saudiData.forEach(d => { saudiCumulativeXG += d.xG; d.cumulativeXG = saudiCumulativeXG; });
-
-  // Merge the two arrays and sort by minute
-  const combinedData = [...argentinaData, ...saudiData].sort((a, b) => a.Minute - b.Minute);
-
-  // Create scales
-  const xScale = d3.scaleLinear()
-    .domain([0, d3.max(combinedData, d => d.Minute)])
+  // X axis
+  const x = d3.scaleLinear()
+    .domain([0, d3.max(top10Data, d => d.total_goals)])
     .range([0, width]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .style("text-anchor", "middle") // Change text-anchor to middle
+      .attr("dx", "0em") // Remove the dx shift
+      .attr("dy", "1em") // Adjust vertical spacing to move text below ticks
+      .attr("transform", ""); // Remove the rotate transformation
 
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(combinedData, d => d.cumulativeXG)])
-    .range([height, 0]);
-
-  // Add X axis
+  // Y axis
+  const y = d3.scaleBand()
+    .range([0, height])
+    .domain(top10Data.map(d => d.country))
+    .padding(0.1);
   svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale).ticks(5));
+    .call(d3.axisLeft(y));
 
-  // Add Y axis
-  svg.append("g")
-    .call(d3.axisLeft(yScale).ticks(5));
+  // Bars
+  svg.selectAll(".bar")
+    .data(top10Data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", x(0))
+    .attr("y", d => y(d.country))
+    .attr("width", d => x(d.total_goals))
+    .attr("height", y.bandwidth())
+    .attr("fill", "#69b3a2")
+    .on("mouseover", function(event, d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+      tooltip.html(d.country + "<br/>" + "Goals: " + d.total_goals)
+        .style("left", (event.pageX) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
 
-  // Create line generators
-  const argentinaLine = d3.line()
-    .x(d => xScale(d.Minute))
-    .y(d => yScale(d.cumulativeXG))
-    .curve(d3.curveStepAfter); // Use step after curve for the step effect
 
-  const saudiLine = d3.line()
-    .x(d => xScale(d.Minute))
-    .y(d => yScale(d.cumulativeXG))
-    .curve(d3.curveStepAfter); // Use step after curve for the step effect
-
-  // Add Argentina's line path
-  svg.append("path")
-    .data([argentinaData])
-    .attr("fill", "none")
-    .attr("stroke", "blue") // Replace with Argentina's color
-    .attr("stroke-width", 1.5)
-    .attr("d", argentinaLine);
-
-  // Add Saudi Arabia's line path
-  svg.append("path")
-    .data([saudiData])
-    .attr("fill", "none")
-    .attr("stroke", "green") // Replace with Saudi Arabia's color
-    .attr("stroke-width", 1.5)
-    .attr("d", saudiLine);
-
-  // Add points for Argentina's shots
-  svg.selectAll(".dot-argentina")
-    .data(argentinaData)
-    .enter()
-    .append("circle")
-      .attr("cx", d => xScale(d.Minute))
-      .attr("cy", d => yScale(d.cumulativeXG))
-      .attr("r", 4)
-      .attr("fill", "yellow"); // Use different color for goals if needed
-
-  // Define the div for the tooltip
-  const div = d3.select("body").append("div")	
-      .attr("class", "tooltip")				
-      .style("opacity", 0);
-
-  // Add the points with tooltips for Argentina's shots
-  svg.selectAll(".dot-argentina")
-    .data(argentinaData)
-    .enter()
-    .append("circle")
-      .attr("cx", d => xScale(d.Minute))
-      .attr("cy", d => yScale(d.cumulativeXG))
-      .attr("r", 5)
-      .attr("fill", "#377eb8")
-      .on("mouseover", function(event, d) {
-          div.transition()		
-              .duration(200)		
-              .style("opacity", .9);		
-          div.html("Minute: " + d.Minute + "<br/>"  + "xG: " + d.xG)
-              .style("left", (event.pageX) + "px")		
-              .style("top", (event.pageY - 28) + "px");
-          })					
-      .on("mouseout", function(d) {		
-          div.transition()		
-              .duration(500)		
-              .style("opacity", 0);	
-      });
-
-  // Add the points with tooltips for Saudi Arabia's shots
-  svg.selectAll(".dot-saudi")
-    .data(saudiData)
-    .enter()
-    .append("circle")
-      .attr("cx", d => xScale(d.Minute))
-      .attr("cy", d => yScale(d.cumulativeXG))
-      .attr("r", 5)
-      .attr("fill", "#4daf4a")
-      .on("mouseover", function(event, d) {
-          div.transition()		
-              .duration(200)		
-              .style("opacity", .9);		
-          div.html("Minute: " + d.Minute + "<br/>"  + "xG: " + d.xG)
-              .style("left", (event.pageX) + "px")		
-              .style("top", (event.pageY - 28) + "px");
-          })					
-      .on("mouseout", function(d) {		
-          div.transition()		
-              .duration(500)		
-              .style("opacity", 0);	
-      });
-
-  // Add a legend (optional)
-  // Add one dot in the legend for each name.
-  const size = 20
-  const allGroups = ["Argentina", "Saudi Arabia"]
-  svg.selectAll("myrect")
-    .data(allGroups)
-    .enter()
-    .append("circle")
-      .attr("cx", 100)
-      .attr("cy", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
-      .attr("r", 7)
-      .style("fill", function(d){ return color(d)})
-
-  // Add one dot in the legend for each name.
-  svg.selectAll("mylabels")
-    .data(allGroups)
-    .enter()
-    .append("text")
-      .attr("x", 100 + size*.8)
-      .attr("y", function(d,i){ return 10 + i * (size + 5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-      .style("fill", function(d){ return color(d)})
-      .text(function(d){ return d})
-      .attr("text-anchor", "left")
-      .style("alignment-baseline", "middle")
+    svg.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", height + margin.bottom/ 1.3)  // Adjust this to position the text below the chart
+    .attr("text-anchor", "middle")  
+    .style("font-size", "15px") 
+    .style("font-weight", "bold")
+    .text("Top 10 countries based on their total goals in the World Cup.");
 });
