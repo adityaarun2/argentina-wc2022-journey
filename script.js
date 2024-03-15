ddfd4b6b08018b6abc285baac1c80b7dd5840b24
Ari Juljulian <arijul2@me.com>
2024-03-15 13:20:17 -0700

    added shot data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the dimensions and margins of the graph
-const margin = { top: 20, right: 20, bottom: 70, left: 100 },
+const margin = { top: 30, right: 30, bottom: 30, left: 60 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
@@ -9,62 +9,76 @@ const svg = d3.select("#bar-chart")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Create a tooltip
-var tooltip = d3.select("body").append("div")
+const tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-// Parse the Data
-d3.json('total_world_cup_goals_by_country.json').then(function(data) {
+// Load the CSV data
+d3.csv('Argentina Shot Data - Saudi Arabia.csv').then(function(data) {
+  
+  // Convert string numbers to actual numbers and calculate cumulative xG
+  let cumulativeXG = 0;
+  data.forEach(d => {
+    d.minute = +d.Minute;
+    d.xG = +d.xG;
+    cumulativeXG += d.xG;
+    d.cumulativeXG = cumulativeXG;
+  });
 
-  // Sort the data by total goals
-  data.sort(function(a, b) { return d3.descending(a.total_goals, b.total_goals); });
-
-  // X axis
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.total_goals; })])
+  // Create scales
+  const xScale = d3.scaleLinear()
+    .domain([0, 90]) // Assuming 90 minutes in a game
     .range([0, width]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-90)");
 
-  // Y axis
-  const y = d3.scaleBand()
-    .range([0, height])
-    .domain(data.map(function(d) { return d.country; }))
-    .padding(0.1);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.cumulativeXG)])
+    .range([height, 0]);
 
-  // Bars
-  svg.selectAll(".bar")
+  // Add X axis
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale).ticks(6));
+
+  // Add Y axis
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
+
+  // Draw the cumulative xG step line
+  const line = d3.line()
+    .x(d => xScale(d.minute))
+    .y(d => yScale(d.cumulativeXG))
+    .curve(d3.curveStepAfter);
+
+  svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "purple") // Change the color to match your design
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+
+  // Add points for each shot
+  svg.selectAll(".shot-point")
     .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.country); })
-    .attr("width", function(d) { return x(d.total_goals); })
-    .attr("height", y.bandwidth())
-    .attr("fill", "#69b3a2")
-    .on("mouseover", function(event, d) {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity", .9);
-      tooltip.html(d.country + "<br/>" + "Goals: " + d.total_goals)
-        .style("left", (event.pageX) + "px")
-        .style("top", (event.pageY - 28) + "px");
-    })
-    .on("mouseout", function(d) {
-      tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-    });
+    .enter().append("circle")
+      .attr("class", "shot-point")
+      .attr("cx", d => xScale(d.minute))
+      .attr("cy", d => yScale(d.cumulativeXG))
+      .attr("r", 5) // Size of the circle
+      .attr("fill", d => d.Outcome === "Goal" ? "green" : "red")
+      .on("mouseover", function(event, d) {
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", 1); // Make tooltip visible
+        tooltip.html(`Player: ${d.Player}<br>xG: ${d.xG}<br>Outcome: ${d.Outcome}`)
+          .style("left", (event.pageX) + "px")
+          .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip.transition()
+          .duration(500)
+          .style("opacity", 0); // Hide tooltip
+      });
 });
