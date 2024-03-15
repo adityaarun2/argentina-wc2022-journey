2d3f31b6967ae8955a9cb7699a7991dd3f03cd1a
Ari Juljulian <arijul2@me.com>
2024-03-15 13:36:58 -0700

    idk

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Set the dimensions and margins of the graph
-const margin = { top: 30, right: 30, bottom: 30, left: 60 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+// Assuming the CSV file is correctly formatted and available at the given path
 
-// Append the svg object to the div called 'bar-chart'
-const svg = d3.select("#bar-chart")
+// Set the dimensions and margins of the graph
+const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+// Append the svg object to the div called 'xg-chart'
+const svg = d3.select("#xg-chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -16,69 +18,43 @@ const tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-// Load the CSV data
+// Read the data
 d3.csv('Argentina Shot Data - Saudi Arabia.csv').then(function(data) {
-  
-  // Convert string numbers to actual numbers and calculate cumulative xG
-  let cumulativeXG = 0;
-  data.forEach(d => {
-    d.minute = +d.Minute;
-    d.xG = +d.xG;
-    cumulativeXG += d.xG;
-    d.cumulativeXG = cumulativeXG;
-  });
 
-  // Create scales
-  const xScale = d3.scaleLinear()
-    .domain([0, 90]) // Assuming 90 minutes in a game
-    .range([0, width]);
+  // Group the data: I want to draw one line per group
+  const sumstat = d3.group(data, d => d.Squad);
 
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.cumulativeXG)])
-    .range([height, 0]);
-
-  // Add X axis
+  // Add X axis --> it is a date format
+  const x = d3.scaleLinear()
+    .domain([0, 90])
+    .range([ 0, width ]);
   svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale).ticks(6));
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
   // Add Y axis
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return +d.xG; })])
+    .range([ height, 0 ]);
   svg.append("g")
-    .call(d3.axisLeft(yScale));
+    .call(d3.axisLeft(y));
 
-  // Draw the cumulative xG step line
-  const line = d3.line()
-    .x(d => xScale(d.minute))
-    .y(d => yScale(d.cumulativeXG))
-    .curve(d3.curveStepAfter);
+  // color palette
+  const color = d3.scaleOrdinal()
+    .domain(["Argentina", "Saudi Arabia"])
+    .range(['#377eb8','#4daf4a'])
 
-  svg.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "purple") // Change the color to match your design
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
-
-  // Add points for each shot
-  svg.selectAll(".shot-point")
-    .data(data)
-    .enter().append("circle")
-      .attr("class", "shot-point")
-      .attr("cx", d => xScale(d.minute))
-      .attr("cy", d => yScale(d.cumulativeXG))
-      .attr("r", 5) // Size of the circle
-      .attr("fill", d => d.Outcome === "Goal" ? "green" : "red")
-      .on("mouseover", function(event, d) {
-        tooltip.transition()
-          .duration(200)
-          .style("opacity", 1); // Make tooltip visible
-        tooltip.html(`Player: ${d.Player}<br>xG: ${d.xG}<br>Outcome: ${d.Outcome}`)
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-        tooltip.transition()
-          .duration(500)
-          .style("opacity", 0); // Hide tooltip
-      });
+  // Draw the line
+  svg.selectAll(".line")
+      .data(sumstat)
+      .join("path")
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return color(d[0]) })
+        .attr("stroke-width", 1.5)
+        .attr("d", function(d){
+          return d3.line()
+            .x(function(d) { return x(d.Minute); })
+            .y(function(d) { return y(+d.xG); })
+            (d[1])
+        })
 });
